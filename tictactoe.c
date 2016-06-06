48463c8c9dd1ac189b42760ae5d5929f8930b653
REBECA CRISTINA LIMA GOMES <16104083@l1801307.portoalegre.pucrsnet.br>
2016-06-06 11:19:27 -0300

    modificação das cores, colocação de um nome para o jogador 1, tranformação da possibilidade de escolha do número de jogos, modificação noresultado final do jogo, adição de bibliotecas no programa

--- tictactoe.c
+++ tictactoe.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include "conio.h"
+#include <string.h> 
 
 int posimax, posimin;
 
@@ -6,22 +8,22 @@ int posimax, posimin;
 void print_grade(char b[10])
 {
     printf("%c|%c|%c\n",b[1],b[2],b[3]);
-  
+
     printf("%c|%c|%c\n",b[4],b[5],b[6]);
-    
+
     printf("%c|%c|%c\n",b[7],b[8],b[9]);
 }
 
 int checaVitoria(char grade[])
 {
     if ((grade[1] == 'X' && grade[2] == 'X' && grade[3] == 'X') ||
-        (grade[4] == 'X' && grade[5] == 'X' && grade[6] == 'X') ||
-        (grade[7] == 'X' && grade[8] == 'X' && grade[9] == 'X') ||
-        (grade[1] == 'X' && grade[4] == 'X' && grade[7] == 'X') ||
-        (grade[2] == 'X' && grade[5] == 'X' && grade[8] == 'X') ||
-        (grade[3] == 'X' && grade[6] == 'X' && grade[9] == 'X') ||
-        (grade[1] == 'X' && grade[5] == 'X' && grade[9] == 'X') ||
-        (grade[3] == 'X' && grade[5] == 'X' && grade[7] == 'X'))
+            (grade[4] == 'X' && grade[5] == 'X' && grade[6] == 'X') ||
+            (grade[7] == 'X' && grade[8] == 'X' && grade[9] == 'X') ||
+            (grade[1] == 'X' && grade[4] == 'X' && grade[7] == 'X') ||
+            (grade[2] == 'X' && grade[5] == 'X' && grade[8] == 'X') ||
+            (grade[3] == 'X' && grade[6] == 'X' && grade[9] == 'X') ||
+            (grade[1] == 'X' && grade[5] == 'X' && grade[9] == 'X') ||
+            (grade[3] == 'X' && grade[5] == 'X' && grade[7] == 'X'))
     {
         return 1;
     }
@@ -42,9 +44,9 @@ int checaVitoria(char grade[])
 int checaEmpate(char grade[])
 {
     if ((checaVitoria(grade) == 0) && (grade[1] != ' ') && (grade[2] != ' ') &&
-        (grade[3] != ' ') && (grade[4] != ' ') && (grade[5] != ' ') &&
-        (grade[6] != ' ') && (grade[7] != ' ') && (grade[8] != ' ') &&
-        (grade[9] != ' '))
+            (grade[3] != ' ') && (grade[4] != ' ') && (grade[5] != ' ') &&
+            (grade[6] != ' ') && (grade[7] != ' ') && (grade[8] != ' ') &&
+            (grade[9] != ' '))
     {
         return 1;
     }
@@ -54,19 +56,19 @@ int checaEmpate(char grade[])
 int minimax(int player, char grade[], int n)
 {
     int i, res, j;
-    
+
     int max = -999;
     int min = 999;
-    
+
     int final = checaVitoria(grade);
-   
-if (final == 1)
+
+    if (final == 1)
         return 1;
     else if (final == (-1))
         return -1;
     else if (final = checaEmpate(grade))
         return 0;
-    
+
     for (i = 1; i < 10; i++)
     {
         if(grade[i] == ' ')
@@ -75,8 +77,9 @@ if (final == 1)
             {
                 grade[i] = 'O';
                 res = minimax(1, grade, n + 1);
-                
+
                 grade[i] = ' ';
+
                 if(res < min)
                 {
                     min = res;
@@ -98,86 +101,132 @@ if (final == 1)
             }
         }
     }
-    
+
     if (player == 1)
         return max;
     else return min;
 }
- 
+
 
 
 int main()
 {
-    int i, posicao, cont, final,jogada;
-    char grade[10];
-    
-    for(i = 1; i < 10; i++)
-        grade[i] = ' ';
-    
-    printf("Jogo da Velha:\n");
-    print_grade(grade);
-    
-    for(i = 1; i < 10; i++)
-    {
-        final = checaVitoria(grade);
-        if (final == 1)
-        {
-            printf("O jogador X ganhou!\n");
-            break;
-        }
-        else if (final == -1)
-        {
-            printf("O jogador O ganhou!\n");
-            break;
-        }
+	int GdBgColor = 6;
+	system("clear");
+    	setbgrcolor(GdBgColor);
+    	int i, partidas, posicao, cont, final,jogada, soma1 = 0, soma2 = 0;
+    	char grade[10];
+   	char nome[50];
+	
+	puts("Forneça seu nome: ");
+	fgets(nome, 49, stdin);
+	nome[strlen(nome)- 1] = '\0';
 
-        if (i % 2 == 1)
-	{            
-            	cont = 0;
-		
+	printf("informe o número de partidas que você quer jogar: ");
+	scanf("%i", &partidas);
+	
+     	while ( partidas != 0)
+    	{
 
-		while ( cont != 1)
+    		for(i = 1; i < 10; i++)
+   		{
+      			grade[i] = ' ';
+    		}
+
+	printf("Jogo da Velha:\n");
+	
+	print_grade(grade);
+
+		for(i = 1; i < 10; i++)
+   		{
+        		final = checaVitoria(grade);
+        		
+			if (final == 1)
+        		{
+           			printf("O jogador X ganhou essa partida!\n");
+	    			printf("%s\n", nome);
+				soma1 = soma1 + 1;
+            			break;
+        		}
+        		else if (final == -1)
+        		{
+           			printf("O jogador O ganhou essa partida!\n");
+				soma2 = soma2 + 1;
+	    			break;
+        		}
+
+        		if (i % 2 == 1)
+        		{
+            			cont = 0;
+
+
+            			while ( cont != 1)
+            			{
+                			printf("Jogada do jogador %s:\n", nome);
+                			scanf("%d", &posicao);
+
+	                		if(( posicao <= 9) && (posicao >= 1) && (grade[posicao] == ' '))
+                			{
+                    				grade[posicao] = 'X';
+                    				cont = 1;
+                			}
+                			else
+                			{
+                    				printf("jogada invalida, tente novamente...\n");
+                			}
+
+            			}
+        		}
+
+
+        	if ((final == 0) && (i != 9))
+        	{
+            	posimax = -1;
+            	posimin = -1;
+            		if(i % 2 == 1)
+            		{
+                		jogada = minimax(1, grade, 0);
+               		 	//printf("A melhor jogada para o jogador X e: %d\n", posimax);
+           		}
+            		else
+            		{
+                		jogada = minimax(2, grade, 0);
+                		grade[posimin] = 'O';
+                		print_grade(grade);
+
+            		}
+        	}
+        	else 
 		{
-			printf("Jogada do jogador X:\n");
-			scanf("%d", &posicao);
-			
-		
-			if(( posicao <= 9) && (posicao >= 1) && (grade[posicao] == ' '))
-			{  
-	   			grade[posicao] = 'X';
-				cont = 1;
-			}
-			else 
-			{
-				printf("jogada invalida, tente novamente...\n");
-			}        		
-            		
-		}
-        }         
-               
-        
-        if ((final == 0) && (i != 9))
-        {
-            posimax = -1;
-            posimin = -1;
-            if(i % 2 == 1)
-            {
-                jogada = minimax(1, grade, 0);
-                //printf("A melhor jogada para o jogador X e: %d\n", posimax);
-            }
-            else
-            {
-                jogada = minimax(2, grade, 0);
-                grade[posimin] = 'O';
-                print_grade(grade);
-                
-            }
-        }
-        else{
-            printf("Deu velha!\n");
-            print_grade(grade);
-        }
+        		printf("Deu velha!\n");
+            		print_grade(grade);
+        	}
 
-    }
+    		}
+	
+		partidas = partidas - 1;
+	}
+
+	printf( "O jogador %s venceu %i vezes e o jogador O venceu %i vezes\n", nome, soma1, soma2);
+	
+	
+	if ( soma1 > soma2)
+	{
+		printf("Jogador %s é o vencedor dos jogos\n", nome);
+	}
+	else
+	{
+		if ( soma2 > soma1)
+		{
+			printf ("Jogador O é o vencedor dos jogos\n");
+		}
+		else
+		{
+			printf("Empate\n");
+		}
+	}
+
+	printf("FIM DO JOGO\n");
+
     return 0;
 }
