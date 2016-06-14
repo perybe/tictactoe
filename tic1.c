ddc7eb6737bea0e6d8e595c9eb2b2d6623bdf3e5
REBECA CRISTINA LIMA GOMES <16104083@l1846971.portoalegre.pucrsnet.br>
2016-06-14 14:58:44 -0300

    #12

--- tic1.c
+++ tic1.c
@@ -1,8 +1,9 @@
 #include "libtic.h"
 #include <stdio.h>
 
+
 void print_grade(char b[10])
-{		//criando o  "tabuleiro"
+{	//criando o  "tabuleiro"
 	printf(" %c |%c |%c\n",b[1],b[2],b[3]);
 	printf(" --+--+--\n");
 	printf(" %c |%c |%c\n",b[4],b[5],b[6]);
